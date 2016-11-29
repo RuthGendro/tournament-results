90ce8677c392831fd08f3ac0fcfb10e55188a351
Ruth Gendro <ruthjg90@gmail.com>
2016-11-29 09:34:39 -0800

    added test for deletePlayers()

--- tournament_test.py
+++ tournament_test.py
@@ -7,3 +7,8 @@ from tournament import *
 def testDeleteMatches():
     deleteMatches()
     print "1. Old matches can be deleted."
+
+def testDelete():
+deleteMatches()
+deletePlayers()
+print "2. Player records can be deleted."
